acb130485ba52bc6a551594102797cbc70034108
Sahana D <sahanagowda5690@gmail.com>
2026-06-03 22:31:31 +0530

    Create Removing Stars From a String.py

---
+++ Removing Stars From a String.py
@@ -0,0 +1,11 @@
+class Solution:
+    def removeStars(self, s):
+        stack = []
+
+        for ch in s:
+            if ch == '*':
+                stack.pop()
+            else:
+                stack.append(ch)
+
+        return ''.join(stack)
