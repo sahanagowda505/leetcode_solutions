752a2d958ee0e242ef0de25b7ff75fd969345e5b
Sahana D <sahanagowda5690@gmail.com>
2026-06-05 19:29:59 +0530

    Create Decode String.py

---
+++ Decode String.py
@@ -0,0 +1,21 @@
+class Solution:
+    def decodeString(self, s):
+        stack = []
+        curr_num = 0
+        curr_str = ""
+
+        for ch in s:
+            if ch.isdigit():
+                curr_num = curr_num * 10 + int(ch)
+            elif ch == '[':
+                stack.append((curr_str, curr_num))
+                curr_str = ""
+                curr_num = 0
+            elif ch == ']':
+                prev_str, num = stack.pop()
+                curr_str = prev_str + num * curr_str
+            else:
+                curr_str += ch
+
+        return curr_str
+        return curr_str
